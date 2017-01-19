4d0b1e4a4822e4f3a3bf5060e2b0d232f4521473
Sergio Ortiz <sir.orman@gmail.com>
2017-01-19 19:33:28 +0100

    update footer.php
    falta poner el estilo en el css comun

--- githalp/footer.php
+++ githalp/footer.php
@@ -1,3 +1,32 @@
 <div id="footer">
-	<p>Aquí debeis añadir los enlaces típicos de "Sobre nosotros", "Ayuda", etcétera.</p>
+	<ul>
+		<li> <a href="#"> <img src="github.png" class="navIcon"/> GitHalp &copy; 2017 </a></li>
+
+		<li style="display:inline;"><a href="index.php"> <img src="inicio.png" class="navIcon"/> Inicio </a></li>
+		<li style="display:inline;"><a href=""> <img src="contact.png" class="navIcon"/> Contáctanos </a></li>
+		<li style="display:inline;"><a href="ayuda.php"> <img src="question1.png" class="navIcon"/> Ayuda </a></li>
+		<li style="display:inline;"><a href="acerca.php">  <img src="about.png" class="navIcon"/> Acerca de </a></li>
+	</ul>
+
+	<!-- Pasar al estilo -->
+	<style>
+		ul {
+		    margin: 0;
+		    padding: 0;
+		    list-style-type: none;
+		}
+
+		li a {
+		    display: block;
+		    text-align: center;
+		    padding: 16px;
+		    text-decoration: none;
+		}
+
+		li a:hover{
+			background-color: #f0f0f0;
+		}
+
+	</style>
+
 </div>
