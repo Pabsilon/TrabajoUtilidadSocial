1595960904dc63927e9f4e03a55ace2de47d74b5
jotagalilea <jgmoreno@ucm.es>
2017-02-27 17:49:07 +0100

    Arreglado el diseño responsive

--- githalp/contacto.php
+++ githalp/contacto.php
@@ -1,12 +1,13 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" type="image/png" href="logo.png" />
 	<link rel="stylesheet" type="text/css" href="estilo.css" />
 	<link rel="stylesheet" type="text/css" href="estiloDropdown.css" />
 	<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet"> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+	 
 
 	<title>GitHalp</title>
 	
