047885c3851c3531849faa953b4a399763dcb78c
Sergio Ortiz <sir.orman@gmail.com>
2017-07-03 12:13:29 +0200

    Pagina de github student

---
+++ githalp/estudiantes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" type="image/png" href="logo.png" />
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<link rel="stylesheet" type="text/css" href="estiloDropdown.css" />
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet"> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>GitHalp</title>
+	
+</head>
+<body>
+	<?php
+		require_once 'header.php';
+	?>
+
+
+	<div id="contenedor">
+		<div id="centro">
+			<h1>GitHub Student</h1>
+			<?php
+				require_once 'contenido/estudiantes.html';
+			?>
+		</div>
+	</div>
+
+
+
+	<?php
+		require_once 'footer.php';
+	?>
+
+</div>
+</body>
+</html>
